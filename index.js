21689eb75a0cf73607a7c95d922f960e14ce0c46
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-02-13 00:58:27 -0400

    feat: config prettier

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get(`/`, (req, res) => {
+  res.send(`Hello World!`);
 });
 
 app.listen(3000, () => {
-  console.log('Listen project port 3000');
+  console.log(`Listen project port 3000`);
 });
